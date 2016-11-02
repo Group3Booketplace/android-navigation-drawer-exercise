d8e8c490e8783a0b08a8dfc7a8aa56754129471c
DUONGTHOAI <jupiter.at@gmail.com>
2016-11-02 19:55:41 +0700

    add toolbar milestone 5

--- app/src/main/java/com/codepath/android/navigationdrawerexercise/activities/MainActivity.java
+++ app/src/main/java/com/codepath/android/navigationdrawerexercise/activities/MainActivity.java
@@ -2,6 +2,7 @@ package com.codepath.android.navigationdrawerexercise.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.codepath.android.navigationdrawerexercise.R;
 
@@ -12,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
